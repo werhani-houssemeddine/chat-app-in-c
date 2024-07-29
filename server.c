c142f28d30c2983ee5e1cbb29d21aa568fcc7cdf
Houssemeddine Werhani <houssemwuerhani@gmail.com>
2024-07-29 14:39:51 +0100

    - make the app variable global. - call the app.listen with the new paramerter(HOST) - create handle_incoming_data function. - call app.recv method.

--- server.c
+++ server.c
@@ -1,13 +1,20 @@
 #include "headers/tcp.h"
 
+struct Server app;
+
+void handle_incoming_data(void *buffer) {
+	printf("Incoming data: %s\n", (char *)buffer);
+}
+
 void handle_incoming_requests(SOCKET sd) {
 	printf("socket descriptor: %d\n", sd);
+	app.recv(sd, handle_incoming_data);
 }
 
 int main() {
-	const struct Server app = Server;
-
-	app.listen("3000", handle_incoming_requests);
+	app = Server;
+	
+	app.listen("127.0.0.1", "3000", handle_incoming_requests);
 	
 	return 0;
 }
