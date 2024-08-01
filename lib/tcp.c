ff1f08d341f9ff9d22007e42e149a537866dd78d
Houssemeddine Werhani <houssemwuerhani@gmail.com>
2024-08-01 23:43:24 +0100

    Refactore tcp.c code. - fn: (clean_socket, accept_socket, connect_socket) add new functions. - fn: handle_error, remove space from the message. - fn: listen_socket, make the fn argument constant. - fn: listen_socket, use localhost as default host, in case of null value. - fn: listen_socket, clean the socket in the end. - add connect_socket to Server struct.

--- lib/tcp.c
+++ lib/tcp.c
@@ -8,9 +8,19 @@ void initSocketForWindows() {
 	#endif
 }
 
+void clean_socket(SOCKET socket) {
+	//? Close socket
+	CLOSE_SOCKET(socket);
+	
+	//? Clean up winsock for windows OP
+	#if defined(_WIN32)
+		WSACleanup();
+	#endif
+}
+
 void handle_error(int condition, char* error) {
 	if(condition) {
-		fprintf(stderr, "%s Error code: %d\n", error, GET_SOCKET_ERRNO() );
+		fprintf(stderr, "%sError code: %d\n", error, GET_SOCKET_ERRNO() );
 		exit(EXIT_FAILURE);
 	}
 }
@@ -38,11 +48,29 @@ void bind_socket(struct addrinfo *addr, SOCKET sd) {
 	handle_error(b, "fn: bind_socket failed. \n");
 }
 
-void listen_socket(char* host, char* PORT, void (*cb)(SOCKET sd)) {
+SOCKET accept_socket(SOCKET sd) {
+	struct sockaddr_storage client_address;
+	socklen_t socket_client_len = sizeof(client_address);
+
+	SOCKET s = accept(sd, (struct sockaddr*) &client_address, &socket_client_len);
+	handle_error(!IS_VALID_SOCKET(s), "fn: accept_socket failed .\n");
+
+	return s;
+}
+
+void listen_socket(const char* host, const char* PORT, void (*cb)(SOCKET sd)) {
+	initSocketForWindows();
+
 	struct addrinfo hints = make_address_hints();
 	struct addrinfo *bind_address = NULL;
-	
-	getaddrinfo(host, PORT, &hints, &bind_address);
+	 
+	//? Use "localhost" (127.0.0.1) as the default host (IP) if
+	//? the provided host (function parameter) is NULL.
+	if(host == NULL) {
+		getaddrinfo("127.0.1.1", PORT, &hints, &bind_address);
+	} else {
+		getaddrinfo(host, PORT, &hints, &bind_address);
+	}
 	
 	printf("Make socket ...\n");
 	SOCKET sd = make_socket(bind_address);
@@ -58,8 +86,68 @@ void listen_socket(char* host, char* PORT, void (*cb)(SOCKET sd)) {
 
 	printf("Handle incoming requests...\n");
 	cb(sd);
+
+	printf("Close socket and clean up...\n");
+	clean_socket(sd);
+}
+
+struct addrinfo make_hints_remote_addr() {
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM;
+
+	return hints;
+}
+
+void get_remote_address(struct addrinfo *peer_address) {
+	char address_buffer[1000];
+	char service_buffer[1000];
+
+	getnameinfo(
+		peer_address->ai_addr,
+		peer_address->ai_addrlen,
+		address_buffer,
+		sizeof(address_buffer),
+		service_buffer,
+		sizeof(service_buffer),
+		NI_NUMERICHOST
+	);
+
+	printf("%s %s.\n", address_buffer, service_buffer);
+}
+
+void connection(SOCKET sd, struct addrinfo *peer_address) {
+	int c = connect(sd, peer_address->ai_addr, peer_address->ai_addrlen);
+	handle_error(c, "fn: connection failed.\n");
+}
+
+void connect_socket(const char *host, const char *port, void (*cb)(SOCKET sd)) {
+	initSocketForWindows();
+
+	struct addrinfo hints = make_hints_remote_addr();
+	struct addrinfo *peer_address;
+
+
+	printf("Config remote address ...\n");
+	getaddrinfo(host, port, &hints, &peer_address);
+
+	printf("Get remote address ...\n");
+	get_remote_address(peer_address);
+
+	printf("Make socket ...\n");
+	SOCKET sd = make_socket(peer_address);
+
+	printf("Connecting ...\n");
+	connection(sd, peer_address);
+
+	printf("free memory from 'peer_address' variable.\n");
+	freeaddrinfo(peer_address);
+
+	cb(sd);
+
 }
 
 const struct Server Server = { 
-	.listen = listen_socket 
+	.listen  = listen_socket,
+	.connect = connect_socket
 };
